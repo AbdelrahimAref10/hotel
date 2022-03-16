432779861e2c77bd0c7248750c9924663ab3b522
AbdelrahimAref10 <abdelrahimaref18@gmail.com>
2022-03-16 19:21:26 +0200

    ccc

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,18 @@ var winH = $(window).height(),
 $('.slider').height(winH - (navH));
 $('.slider-2').height('300px');
 
+$("html").niceScroll({
+    cursorcolor:"#dca73a",
+});
+
+/*** Magnifib popup for photo section  ***/
+$(function(){
+    $('.work').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery: {
+        enabled:true
+      }
+      // other options
+    });
+});
